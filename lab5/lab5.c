da133d0f4428297c19c7d6d42e8a5209dc18ceb9
jessejoseph3 <jjoseph@hmc.edu>
2015-10-11 23:53:56 -0700

    added timers and main function files

---
+++ lab5/lab5.c
@@ -0,0 +1,26 @@
+//lab5.c
+//uses GPIO to play fur elise
+//jjoseph@hmc.edu
+
+#include <lab5_starter.c>
+#include <lab5_gpio.c>
+#include <lab5_timers.c>
+#include <stdio.h>
+
+#define OUTPUTPIN 5;
+
+void play_note(int freq, int millis){
+	int halfPeriodMicros = 500000/freq;
+	int numCycles = freq*millis/1000;
+	for(int i = 0; i < numCycles; i++){
+		digitalWrite(OUTPUTPIN,0);
+		delayMicros(halfPeriodMicros);
+		digitalWrite(OUTPUTPIN,1);
+		delayMicros(halfPeriodMicros);
+	}
+}
+
+void main(){
+	pioInit();
+	
+}
---
+++ lab5/lab5_timers.c
@@ -0,0 +1,15 @@
+//lab5_timers.c
+//functions for using system timers to set delays
+//jjoseph@hmc.edu
+#include <lab5_starter.c>
+#include <lab5_gpio.c>
+#include <stdio.h>
+
+#define TIMER_BASE (volatile unsigned int *) 0x3F003000
+
+void delayMicros(unsigned int micros)
+{
+	TIMER_BASE[0] = 0b0010;					//clear M1
+	TIMER_BASE[4] = TIMER_BASE[1] + micros;	//set comparison to be CLo + micros
+	while(~TIMER_BASE[0] & 0b0010);			//wait until match
+}
