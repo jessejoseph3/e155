e8e40cd0befcf1b33b5b36bf66dd0bd2577d5a05
jessejoseph3 <jjoseph@hmc.edu>
2015-10-13 13:55:24 -0700

    commented, hardcoded, gave up

--- lab5/lab5.c
+++ lab5/lab5.c
@@ -11,52 +11,53 @@
 #define OUTPUTPIN 5
 
 void play_note(int freq, int millis){
-	if(freq == 0){
+	if(freq == 0){						//for rest note 0Hz, just pause for millis
 		delayMicros(millis*1000);
 	}
 	
 	else{
-		int halfPeriodMicros = 500000/freq;
-		int numCycles = (freq*millis)/1000;
+		int halfPeriodMicros = 500000/freq;		//else find half the period
+		int numCycles = (freq*millis)/1000;		//and the number of cycles that corresponds to duration
 		int i;
-		for(i = 0; i < numCycles; i++){
-			digitalWrite(OUTPUTPIN,0);
+		for(i = 0; i < numCycles; i++){			//iterate fornumber of cycles
+			digitalWrite(OUTPUTPIN,0);			//pin lo for half of the period
 			delayMicros(halfPeriodMicros);
-			digitalWrite(OUTPUTPIN,1);
+			digitalWrite(OUTPUTPIN,1);			//hi for half the period
 			delayMicros(halfPeriodMicros);
 		}
 	}
 }
 
 void main(){
-	pioInit();
-	timerInit();
-	pinMode(OUTPUTPIN,OUTPUT);
-	int songSel;
-	const int **song;
-	printf("Select a song: ");
-	scanf("%d", &songSel);
-	if(songSel == 0){
+	pioInit();				//initialize gpio memory map
+	timerInit();			//initialize timer memory map
+	pinMode(OUTPUTPIN,OUTPUT);	//initialize output pin
+	int songSel;				//will be used to choose song to play
+	printf("Select a song: ");	//prompt user
+	scanf("%d", &songSel);		//get user input
+	if(songSel == 0){			//if user pressed 0, song is Fur Elise
 		printf("\n You selected Fur Elise \n");
-		song = notes;
-		printf("this is a line 1 \n");
 	}
-	else if(songSel == 1){
+	else if(songSel == 1){		//if user pressed 1, song is Song of Time
 		printf("\n You selected the Song of Time \n");
-		song = songOfTime;
 	}
-	else{
+	else{						//else, song is song of Storms
 		printf("\n You selected the Song of Storms \n");
-		song = songOfStorms;
 	}
-	const int *note;
+	const int *note;			//note is an array so must be a pointer
 	int i = 0;
-	printf("this is a line 2 \n");
-	note = *song[0];
-	printf("note[1] is: %d",note[1]);
-	while(!(note[1] == 0)){
-		play_note(note[0],note[1]);
-		note = *song[i];
-		i++;
+	note = emptyNote;			//get first empty note to initialize loop
+	while(!(note[1] == 0)){		//keep going until a 0 duration note is reached
+		if(songSel == 0){		//if fur elise, get fur elise note
+			note = notes[i];
+		}
+		else if(songSel == 1){	//if song of time, get song o ftime note 
+			note = songOfTime[i];
+		}
+		else{					//else get song of storms note
+			note = songOfStorms[i];	
+		}
+		play_note(note[0],note[1]);	//play the note
+		i++;					//increment note
 	}
 }
