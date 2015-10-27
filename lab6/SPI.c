16464f294c8f249db3193285707dc840395ab600
jessejoseph3 <jjoseph@hmc.edu>
2015-10-26 22:42:23 -0700

    changed variable name of spi0 pointer

--- lab6/SPI.c
+++ lab6/SPI.c
@@ -2,7 +2,7 @@
 //SPI functionality for e155 lab6
 
 #include "GPIO.h"
-#define spi0 (volatile unsigned int *) 0x3F204000
+#define SPIBASE (volatile unsigned int *) 0x3F204000
 void SPIinit(int freq, int settings)
 {
 	pinMode(7,ALT0);
@@ -10,16 +10,16 @@ void SPIinit(int freq, int settings)
 	pinMode(10,ALT0);
 	pinMode(11,ALT0);
 
-	spi0[2] = 250000000/freq;
-	spi0[0] = settings;
-	spi0[0] |= 1 << 7;
+	SPIBASE[2] = 250000000/freq;
+	SPIBASE[0] = settings;
+	SPIBASE[0] |= 1 << 7;
 }
 
 char spiXFer(char data_out)
 {
-	spi0[1] = data_out;
-	while(!spi[0]& (1<<4));
-	return spi0[1];
+	SPIBASE[1] = data_out;
+	while(!SPIBASE[0]& (1<<4));
+	return SPIBASE[1];
 }
 
 int main(void)
