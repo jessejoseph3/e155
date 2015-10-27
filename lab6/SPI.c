aab223f995348090eeb2244c69f26e4bc8489b71
jessejoseph3 <jjoseph@hmc.edu>
2015-10-27 13:12:13 -0700

    changed voltage to double

--- lab6/SPI.c
+++ lab6/SPI.c
@@ -66,7 +66,7 @@ int main(void)
 	pioInit();
 	SPIinit(100000,0x30);
 	int data = readSPI();
-	float voltage = 5.0*(data/1024.0);
+	double voltage = 5.0*(data/1024.0);
 	printf("voltage is %d \n", voltage);
 	//printf("%s%c%c\n", "Content-Type:text/html;charset=iso-8859-1",13,10);
 
